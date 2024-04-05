54bb49856d59d91a0d0ad247d1267116925d014c
Léo-Nils Boissier <leo.nils2000@gmail.com>
2024-04-05 22:28:47 +0200

    Add a question type

--- src/models/question.rs
+++ src/models/question.rs
@@ -1,12 +1,19 @@
+enum QuestionType {
+    String,
+    SingleChoice(Vec<String>),
+}
+
 struct Question {
     statement: String,
     answer: Option<String>,
+    question_type: QuestionType,
 }
 
 impl Question {
-    pub fn new(statement: String) -> Question {
+    pub fn new(statement: String, question_type: QuestionType) -> Question {
         Question {
             statement,
+            question_type,
             answer: None,
         }
     }
@@ -15,8 +22,9 @@ impl Question {
         &self.statement
     }
 
-    pub fn answer(&mut self, answer: String) {
+    pub fn set_answer(&mut self, answer: String) -> Result<(), String> {
         self.answer = Some(answer);
+        Ok(())
     }
 
     pub fn get_answer(&self) -> Result<String, String> {
@@ -29,18 +37,47 @@ impl Question {
 
 #[cfg(test)]
 mod tests {
-    use crate::models::question::Question;
+    use crate::models::question::{Question, QuestionType};
 
     #[test]
     fn test_create_a_question_with_a_statement_and_retrieve_it() {
-        let question = Question::new("What is your name?".to_string());
+        let question = Question::new("What is your name?".to_string(), QuestionType::String);
         assert_eq!(question.get_statement(), "What is your name?");
     }
 
     #[test]
     fn test_create_a_string_question_and_answer_it() {
-        let mut question = Question::new("What is your name?".to_string());
-        question.answer("John".to_string());
+        let mut question = Question::new("What is your name?".to_string(), QuestionType::String);
+        question.set_answer("John".to_string()).unwrap();
         assert_eq!(question.get_answer().unwrap(), "John");
+    }
+
+    #[test]
+    fn test_create_a_string_question_and_get_answer_without_answering_shall_return_error() {
+        let question = Question::new("What is your name?".to_string(), QuestionType::String);
+        assert_eq!(question.get_answer().unwrap_err(), "No answer provided");
+    }
+
+    #[test]
+    fn test_create_a_single_choice_question_and_retrieve_possible_answers_shall_get_them_all() {
+        let question = Question::new(
+            "What is your favorite color?".to_string(),
+            QuestionType::SingleChoice(vec!["Red".to_string(), "Blue".to_string()]),
+        );
+        assert_eq!(question.get_statement(), "What is your favorite color?");
+        match question.question_type {
+            QuestionType::SingleChoice(answers) => assert_eq!(answers, vec!["Red", "Blue"]),
+            _ => panic!("Invalid question type"),
+        }
+    }
+
+    #[test]
+    fn test_create_a_single_choice_question_and_answer_it_should_save_answer() {
+        let mut question = Question::new(
+            "What is your favorite color?".to_string(),
+            QuestionType::SingleChoice(vec!["Red".to_string(), "Blue".to_string()]),
+        );
+        question.set_answer("Blue".to_string()).unwrap();
+        assert_eq!(question.get_answer().unwrap(), "Blue");
     }
 }
