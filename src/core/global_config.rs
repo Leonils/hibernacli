7768f0940089fd6cb1cb1b362ba84b34fedaa24b
GauthierDoppler <gauthier.doppler@gmail.com>
2024-04-18 16:18:20 +0200

    Adding projects

--- src/core/global_config.rs
+++ src/core/global_config.rs
@@ -52,11 +52,28 @@ impl GlobalConfig {
 
 impl GlobalConfig {
     fn get_project_by_name(&self, name: &str) -> Option<&Project> {
-        unimplemented!()
+        self.projects.iter().find(|p| p.get_name() == name)
+    }
+
+    fn get_project_by_path(&self, path: &str) -> Option<&Project> {
+        self.projects.iter().find(|p| p.get_location() == path)
     }
 
     pub fn add_project(&mut self, project: Project) -> Result<(), String> {
-        unimplemented!()
+        if self.get_project_by_name(&project.get_name()).is_some() {
+            return Err(format!(
+                "Project with name {} already exists",
+                project.get_name()
+            ));
+        }
+        if self.get_project_by_path(&project.get_location()).is_some() {
+            return Err(format!(
+                "Project with path {} already exists",
+                project.get_location()
+            ));
+        }
+        self.projects.push(project);
+        Ok(())
     }
 
     pub fn remove_project(&mut self, name: &str) -> Result<(), String> {
@@ -377,9 +394,11 @@ mod tests {
                 MockDeviceWithParametersFactory, MockGlobalConfigProviderFactory,
             },
         },
-        models::backup_requirement::{BackupRequirementClass, SecurityLevel},
-        models::project::ProjectTrackingStatus,
-        models::secondary_device::DeviceFactory,
+        models::{
+            backup_requirement::{BackupRequirementClass, SecurityLevel},
+            project::{Project, ProjectTrackingStatus},
+            secondary_device::DeviceFactory,
+        },
     };
 
     use super::GlobalConfig;
@@ -1109,6 +1128,78 @@ type = "MockDeviceWithParameters"
         assert_eq!(
             config.err().unwrap(),
             "Missing backup_requirement_class section"
+        );
+    }
+
+    #[test]
+    fn when_we_add_a_project_to_the_config_it_shall_be_visible() {
+        let mut global_config = GlobalConfig {
+            devices: vec![],
+            projects: vec![],
+        };
+
+        let project = Project::new("MyProject".to_string(), "/tmp".to_string(), None);
+
+        global_config.add_project(project).unwrap();
+        assert_eq!(global_config.projects.len(), 1);
+        assert_eq!(global_config.projects[0].get_name(), "MyProject");
+        assert_eq!(global_config.projects[0].get_location(), "/tmp");
+    }
+
+    #[test]
+    fn when_we_add_multiple_projects_to_the_config_it_shall_be_visible() {
+        let mut global_config = GlobalConfig {
+            devices: vec![],
+            projects: vec![],
+        };
+
+        let project = Project::new("MyProject".to_string(), "/tmp".to_string(), None);
+        let project2 = Project::new("MySecondProject".to_string(), "/root".to_string(), None);
+
+        global_config.add_project(project).unwrap();
+        global_config.add_project(project2).unwrap();
+        assert_eq!(global_config.projects.len(), 2);
+        assert_eq!(global_config.projects[0].get_name(), "MyProject");
+        assert_eq!(global_config.projects[0].get_location(), "/tmp");
+        assert_eq!(global_config.projects[1].get_name(), "MySecondProject");
+        assert_eq!(global_config.projects[1].get_location(), "/root");
+    }
+
+    #[test]
+    fn when_we_add_a_project_with_same_name_it_should_return_error() {
+        let mut global_config = GlobalConfig {
+            devices: vec![],
+            projects: vec![],
+        };
+
+        let project = Project::new("MyProject".to_string(), "/tmp".to_string(), None);
+        let project2 = Project::new("MyProject".to_string(), "/tmp".to_string(), None);
+
+        global_config.add_project(project).unwrap();
+        let result = global_config.add_project(project2);
+        assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap(),
+            "Project with name MyProject already exists"
+        );
+    }
+
+    #[test]
+    fn when_we_add_a_project_with_the_same_path_it_should_return_error() {
+        let mut global_config = GlobalConfig {
+            devices: vec![],
+            projects: vec![],
+        };
+
+        let project = Project::new("MyProject".to_string(), "/tmp".to_string(), None);
+        let project2 = Project::new("MySecondProject".to_string(), "/tmp".to_string(), None);
+
+        global_config.add_project(project).unwrap();
+        let result = global_config.add_project(project2);
+        assert!(result.is_err());
+        assert_eq!(
+            result.err().unwrap(),
+            "Project with path /tmp already exists"
         );
     }
 }
